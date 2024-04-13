bb8184e0d1f116a01048e09bf8f6615a0de15959
p1utoze <1ds21ai001@dsce.edu.in>
2024-04-13 15:03:23 +0530

    feat: initialized settings package with configurations for frontend

---
+++ frontend/settings/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).parent.parent
+
+SHAPEFILES_DIR = PROJECT_ROOT / "shapefiles"
+
+_page_config = dict(
+        page_title="SecurA",
+        page_icon="👮🏻",
+        layout="wide",
+    )
